7b5b4547e713d5ac758e4da27c5c6884afa016db
work.yuvarajv@gmail.com <work.yuvarajv@gmail.com>
2022-07-15 22:58:21 +0530

    string methods examples

---
+++ src/test/java/String_Methods.java
@@ -0,0 +1,34 @@
+
+public class String_Methods {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		/*
+		 * String anotherString = "hello world"; Object objStr = str;
+		 */
+
+		
+
+		/*
+		 * System.out.println(str.charAt(6));
+		 * System.out.println(str.compareTo(anotherString));
+		 */
+		//System.out.println(str.concat("raja"));
+		//System.out.println(str.contains("z"));
+		System.out.println(str.endsWith("g"));
+		System.out.println(str.intern());
+		
+		
+		
+		
+		
+		/*
+		 * System.out.println(str.substring(1,5));
+		 * System.out.println(str.compareTo(anotherString));
+		 * System.out.println(str.compareToIgnoreCase(anotherString));
+		 * System.out.println(str.compareTo(objStr.toString()));
+		 */
+	}
+
+	
+}
